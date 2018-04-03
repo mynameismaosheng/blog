f36232b8f98ddd3349516dcbfdbbe21c287ca21e
Maunsell <982431279@qq.com>
2018-04-03 18:59:58 +0800

    update home

--- src/view/Home/Home.vue
+++ src/view/Home/Home.vue
@@ -1,7 +1,29 @@
 <template>
+  <el-tabs tab-position="right" style="height: 200px;">
+    <el-tab-pane label="最新">
+      <span slot="label">
+        <i class="el-icon-loading tabicon"></i> 最新
+      </span>
+      最新内容
+    </el-tab-pane>
+    <el-tab-pane label="最热">
+      <span slot="label">
+        <i class="el-icon-star-on tabicon"></i> 最新
+      </span>
+      最热内容
+    </el-tab-pane>
+  </el-tabs>
 </template>
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home"
+};
 </script>
+<style>
+.tabicon{
+  color: #e8ea78;
+}
+.is-active .tabicon{
+  color: #409EFF;
+}
+</style>
