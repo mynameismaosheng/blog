ed9636c440f549c8af7f2cdb88da7d7d969d4ef8
mynameismaosheng <982431279@qq.com>
2018-04-09 21:37:40 +0800

    home lists preliminary complete

--- src/view/Home/Home.vue
+++ src/view/Home/Home.vue
@@ -1,29 +1,154 @@
 <template>
-  <el-tabs tab-position="right" style="height: 200px;">
-    <el-tab-pane label="最新">
-      <span slot="label">
-        <i class="el-icon-loading tabicon"></i> 最新
-      </span>
-      最新内容
-    </el-tab-pane>
-    <el-tab-pane label="最热">
-      <span slot="label">
-        <i class="el-icon-star-on tabicon"></i> 最新
-      </span>
-      最热内容
-    </el-tab-pane>
-  </el-tabs>
+  <div>
+    <el-button class="allStop" :class="none" type="primary" size="mini" @click="changCollapse">全部收起</el-button>
+    <el-tabs tab-position="right" v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="最新" name="bestNew">
+        <span slot="label">
+          <i class="el-icon-loading tabicon"></i> 最新
+        </span>
+        <el-collapse v-model="bestNew" @change="handleChange">
+          <el-collapse-item name="1">
+            <template slot="title">
+              一致性 Consistency<i class="header-icon el-icon-info"></i>
+            </template> 
+            <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+            <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+          </el-collapse-item>
+          <el-collapse-item title="反馈 Feedback" name="2">
+            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          </el-collapse-item>
+          <el-collapse-item title="效率 Efficiency" name="3">
+            <div>简化流程：设计简洁直观的操作流程；</div>
+            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+          </el-collapse-item>
+          <el-collapse-item title="可控 Controllability" name="4">
+            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          </el-collapse-item>
+          <el-collapse-item title="反馈 Feedback" name="5">
+            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          </el-collapse-item>
+          <el-collapse-item title="效率 Efficiency" name="6">
+            <div>简化流程：设计简洁直观的操作流程；</div>
+            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+          </el-collapse-item>
+          <el-collapse-item title="可控 Controllability" name="7">
+            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          </el-collapse-item>
+          <el-collapse-item title="反馈 Feedback" name="8">
+            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          </el-collapse-item>
+          <el-collapse-item title="效率 Efficiency" name="9">
+            <div>简化流程：设计简洁直观的操作流程；</div>
+            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+          </el-collapse-item>
+          <el-collapse-item title="可控 Controllability" name="10">
+            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-tab-pane>
+      <el-tab-pane label="最热" name="bestHot">
+        <span slot="label">
+          <i class="el-icon-star-on tabicon"></i> 最新
+        </span>
+        <el-collapse v-model="bestHot" @change="handleChange">
+          <el-collapse-item name="1">
+            <template slot="title">
+              一致性 Consistency<i class="header-icon el-icon-info"></i>
+            </template> 
+            <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+            <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+          </el-collapse-item>
+          <el-collapse-item title="反馈 Feedback" name="2">
+            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          </el-collapse-item>
+          <el-collapse-item title="效率 Efficiency" name="3">
+            <div>简化流程：设计简洁直观的操作流程；</div>
+            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+          </el-collapse-item>
+          <el-collapse-item title="可控 Controllability" name="4">
+            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          </el-collapse-item>
+          <el-collapse-item title="反馈 Feedback" name="5">
+            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          </el-collapse-item>
+          <el-collapse-item title="效率 Efficiency" name="6">
+            <div>简化流程：设计简洁直观的操作流程；</div>
+            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+          </el-collapse-item>
+          <el-collapse-item title="可控 Controllability" name="7">
+            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          </el-collapse-item>
+          <el-collapse-item title="反馈 Feedback" name="8">
+            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          </el-collapse-item>
+          <el-collapse-item title="效率 Efficiency" name="9">
+            <div>简化流程：设计简洁直观的操作流程；</div>
+            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+          </el-collapse-item>
+          <el-collapse-item title="可控 Controllability" name="10">
+            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script>
-export default {
-  name: "Home"
-};
+  export default {
+    name: "Home",
+    data() {
+      return {
+        activeName: 'bestNew',
+        bestNew: ['1'],
+        bestHot: ['6'],
+        none:''
+      }
+    },
+    methods: {
+      handleClick(tab, event){
+        this.none = this[tab.name].length ? '' : 'none'
+      },
+      handleChange(val) {
+        this.none = val.length ? '' : 'none'
+      },
+      changCollapse() {
+        this[this.activeName] = [];
+        this.none = 'none'
+      }
+    }
+  }
 </script>
 <style>
-.tabicon{
+.tabicon {
   color: #e8ea78;
 }
-.is-active .tabicon{
-  color: #409EFF;
+.is-active .tabicon {
+  color: #409eff;
+}
+.allStop{
+  position: fixed;
+  right: 25px;
+  bottom: 20px;
+}
+.none{
+  display: none;
 }
 </style>
