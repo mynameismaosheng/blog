3aa7981aeefb46c7772470d38e69e624a7ee32e2
Maunsell <982431279@qq.com>
2018-06-06 17:02:31 +0800

    home list from data render by v-for

--- src/view/Home/Home.vue
+++ src/view/Home/Home.vue
@@ -7,51 +7,8 @@
           <i class="el-icon-loading tabicon"></i> 最新
         </span>
         <el-collapse v-model="bestNew" @change="handleChange">
-          <el-collapse-item name="1">
-            <template slot="title">
-              一致性 Consistency<i class="header-icon el-icon-info"></i>
-            </template> 
-            <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-            <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-          </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="2">
-            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率 Efficiency" name="3">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-          </el-collapse-item>
-          <el-collapse-item title="可控 Controllability" name="4">
-            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-          </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="5">
-            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率 Efficiency" name="6">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-          </el-collapse-item>
-          <el-collapse-item title="可控 Controllability" name="7">
-            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-          </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="8">
-            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率 Efficiency" name="9">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-          </el-collapse-item>
-          <el-collapse-item title="可控 Controllability" name="10">
-            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          <el-collapse-item v-for="item in lists" :key="item.id" :title="item.title" :name="item.id">
+            <div v-html="item.content"></div>
           </el-collapse-item>
         </el-collapse>
       </el-tab-pane>
@@ -60,51 +17,8 @@
           <i class="el-icon-star-on tabicon"></i> 最新
         </span>
         <el-collapse v-model="bestHot" @change="handleChange">
-          <el-collapse-item name="1">
-            <template slot="title">
-              一致性 Consistency<i class="header-icon el-icon-info"></i>
-            </template> 
-            <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-            <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-          </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="2">
-            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率 Efficiency" name="3">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-          </el-collapse-item>
-          <el-collapse-item title="可控 Controllability" name="4">
-            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-          </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="5">
-            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率 Efficiency" name="6">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-          </el-collapse-item>
-          <el-collapse-item title="可控 Controllability" name="7">
-            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-          </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="8">
-            <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-          </el-collapse-item>
-          <el-collapse-item title="效率 Efficiency" name="9">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-          </el-collapse-item>
-          <el-collapse-item title="可控 Controllability" name="10">
-            <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-            <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+          <el-collapse-item v-for="item in lists" :key="item.id" :title="item.title" :name="item.id">
+            <div v-html="item.content"></div>
           </el-collapse-item>
         </el-collapse>
       </el-tab-pane>
@@ -116,39 +30,61 @@
     name: "Home",
     data() {
       return {
-        activeName: 'bestNew',
-        bestNew: ['1'],
-        bestHot: ['6'],
-        none:''
-      }
+        lists: [{
+            title: "一致性1",
+            id: 1,
+            content: `
+          <p>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</p>
+          <strong>加粗字体</strong>
+          <p>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</p>`
+          },
+          {
+            title: "一致性2",
+            id: 2,
+            content: `
+          <p>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</p>
+          <strong>加粗字体</strong>
+          <p>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</p>`
+          }
+        ],
+        activeName: "bestNew",
+        bestNew: ["1"],
+        bestHot: ["6"],
+        none: ""
+      };
     },
     methods: {
-      handleClick(tab, event){
-        this.none = this[tab.name].length ? '' : 'none'
+      handleClick(tab, event) {
+        this.none = this[tab.name].length ? "" : "none";
       },
       handleChange(val) {
-        this.none = val.length ? '' : 'none'
+        this.none = val.length ? "" : "none";
       },
       changCollapse() {
         this[this.activeName] = [];
-        this.none = 'none'
+        this.none = "none";
       }
     }
-  }
+  };
+
 </script>
 <style>
-.tabicon {
-  color: #e8ea78;
-}
-.is-active .tabicon {
-  color: #409eff;
-}
-.allStop{
-  position: fixed;
-  right: 25px;
-  bottom: 20px;
-}
-.none{
-  display: none;
-}
+  .tabicon {
+    color: #e8ea78;
+  }
+
+  .is-active .tabicon {
+    color: #409eff;
+  }
+
+  .allStop {
+    position: fixed;
+    right: 25px;
+    bottom: 20px;
+  }
+
+  .none {
+    display: none;
+  }
+
 </style>
